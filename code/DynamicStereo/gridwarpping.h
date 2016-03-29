6d543e175302eccfd106266d1344557ba2059525
Hang Yan <yanhangxiong@gmail.com>
2016-03-29 10:05:04 -0500

    Hang

--- code/DynamicStereo/gridwarpping.h
+++ code/DynamicStereo/gridwarpping.h
@@ -14,20 +14,26 @@
 #include <iostream>
 #include "model.h"
 #include "../base/file_io.h"
+#include "../base/depth.h"
 
 namespace dynamic_stereo {
     class GridWarpping {
     public:
         typedef int EnergyType;
-        GridWarpping(const FileIO& file_io_, const std::vector<cv::Mat>& images_, const StereoModel<EnergyType>& model_,
-                     const theia::Reconstruction& reconstruction_, const int downsample_, const int offset_, const int gw = 32, const int gh = 18);
+        GridWarpping(const FileIO& file_io_, const int anchor_, const std::vector<cv::Mat>& images_, const StereoModel<EnergyType>& model_,
+                     const theia::Reconstruction& reconstruction_, const Depth& refDepth_, const int downsample_, const int offset_, const int gw = 32, const int gh = 18);
         //void runWarpping(std::vector<cv::Mat>& result);
         void getGridIndAndWeight(const Eigen::Vector2d& pt, Eigen::Vector4i& ind, Eigen::Vector4d& w) const;
+        void computePointCorrespondence(const int id, std::vector<Eigen::Vector2d>& refPt,
+                                        std::vector<Eigen::Vector2d>& srcPt) const;
     private:
         const FileIO& file_io;
         const std::vector<cv::Mat>& images;
         const StereoModel<EnergyType>& model;
         const theia::Reconstruction& reconstruction;
+        const Depth& refDepth;
+        const int anchor;
+
         const int downsample;
         const int offset;
 
