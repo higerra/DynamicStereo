d12b36615c72ac71bbadd66f1c34ae29b1bdfc8b
higerra <higerra@126.com>
2016-04-22 01:35:55 -0500

    Hang

--- code/matlab/fourierConf.m
+++ code/matlab/fourierConf.m
@@ -11,14 +11,19 @@ end
 
 conf = zeros(h,w);
 disp('Computing confidence...');
+min_colordiff=5;
 for y=1:h
     for x=1:w
         seq = reshape(Is(:,y,x,:), N, c);
+        if max(max(seq)-min(seq)) < min_colordiff
+            conf(y,x) = 0.0;
+            continue;
+        end
         seq=seq-repmat(mean(seq),N,1);
         mag = abs(fft(seq));
         ratio = median(max(mag(tf+1:end,:)) ./ max(mag(1:tf,:)));
         conf(y,x) = 1/(1+exp(-1*alpha*(ratio-beta)));
-        if x==416 && y==146
+        if x==22 && y==68
             disp('Debug...');
             figure; hold on;
             subplot(1,2,1);
