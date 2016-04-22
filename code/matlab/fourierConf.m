df898bb86bedf6887f7b80540732280f43f7f137
higerra <higerra@126.com>
2016-04-22 01:29:28 -0500

    hang

---
+++ code/matlab/fourierConf.m
@@ -0,0 +1,42 @@
+function conf = fourierConf(path,anchor,startid,endid,alpha,beta,tf)
+%%
+temp = imread(sprintf('%s/prewarpb%05d_%05d.jpg',path, anchor, startid));
+[h,w,c] = size(temp);
+N = endid - startid + 1;
+Is = zeros(N,h,w,c);
+disp('loading...');
+for i=startid:endid
+    Is(i-startid+1,:,:,:)=imread(sprintf('%s/prewarpb%05d_%05d.jpg',path, anchor, i));
+end
+
+conf = zeros(h,w);
+disp('Computing confidence...');
+for y=1:h
+    for x=1:w
+        seq = reshape(Is(:,y,x,:), N, c);
+        seq=seq-repmat(mean(seq),N,1);
+        mag = abs(fft(seq));
+        ratio = median(max(mag(tf+1:end,:)) ./ max(mag(1:tf,:)));
+        conf(y,x) = 1/(1+exp(-1*alpha*(ratio-beta)));
+        if x==416 && y==146
+            disp('Debug...');
+            figure; hold on;
+            subplot(1,2,1);
+            plot(seq);
+            subplot(1,2,2);
+            plot(mag);
+            disp(max(mag(tf+ceil(1:N/2),:)) ./ max(mag(1:tf,:)));
+            fprintf('median ratio: %.2f, conf: %.2f\n', ratio, conf(y,x));
+            hold off;
+        end
+    end
+end
+
+figure;
+fg1 = subplot(1,2,1);
+imshow(reshape(Is(end,:,:,:),h,w,c)/256);
+fg2 = subplot(1,2,2);
+imshow(conf);
+linkaxes([fg1 fg2], 'xy');
+
+end
