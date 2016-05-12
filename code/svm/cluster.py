3b07864f294c4643449e81591f7d077a8069f29c
higerra <higerra@126.com>
2016-05-11 22:03:09 -0500

    Hang

--- code/svm/cluster.py
+++ code/svm/cluster.py
@@ -1,13 +1,15 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
 from sklearn.cluster import MeanShift, estimate_bandwidth
+from matplotlib import pyplot as plt
+
 import argparse
 
 parser = argparse.ArgumentParser(prog='cluster', description='Cluster the time sequence')
 parser.add_argument('input')
 
-max_frame = 100
+max_frame = 50
+downsample = 4
 
 args = parser.parse_args()
 
@@ -15,8 +17,8 @@ cap = cv2.VideoCapture(args.input)
 assert(cap.isOpened())
 
 kFrames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) / downsample
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) / downsample
 assert(kFrames >= max_frame)
 
 print "Done. kFrame:{}, width:{}, height:{}".format(kFrames, width, height)
@@ -27,6 +29,7 @@ while True:
     ret, img = cap.read()
     if (not ret) or fCount == max_frame:
         break
+    img = cv2.resize(img, (width, height),0,0,cv2.INTER_CUBIC)
     samples[:, fCount*3:(fCount*3+3)] = img.reshape(-1, 3) / 255.0
     fCount += 1
 print "Done"
@@ -39,9 +42,17 @@ print "Meanshift clustering..."
 #bw = estimate_bandwidth(samples, quantile=0.1, n_samples=height * width / 10)
 bw = 2.0
 print "Done. bw:{}".format(bw)
+
 ms = MeanShift(bandwidth=bw)
 
 print "Clustering..."
 ms.fit(samples)
 labels = ms.labels_
-print "Done. Number of clusters: {}".format(len(np.unique(labels)))
+n_cluster = len(np.unique(labels))
+print "Done. Number of clusters: {}".format(n_cluster)
+
+colorTable = np.random.rand(n_cluster, 3)
+resultVis = np.zeros(height, width, 3)
+for y in range(0,height):
+    for x in range(0, width):
+        resultVis[y, x, :] = colorTable[labels[y*width+x], :]
