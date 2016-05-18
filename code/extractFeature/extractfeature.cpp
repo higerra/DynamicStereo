417d730276fd490a82588ca170536dded2c5108f
Hang Yan <yanhangxiong@gmail.com>
2016-05-18 17:33:20 -0500

    Hang

--- code/extractFeature/extractfeature.cpp
+++ code/extractFeature/extractfeature.cpp
@@ -3,65 +3,183 @@
 //
 
 #include "extracfeature.h"
-#include "../external/segment_ms/ms.h"
 
 using namespace std;
 using namespace cv;
+using namespace Eigen;
 
 namespace dynamic_stereo{
-    cv::Size importData(const std::string& path, std::vector<std::vector<float> >& array, const int downsample, const int tWindow){
-        VideoCapture cap(path);
-        CHECK(cap.isOpened());
-        int width = (int)cap.get(CV_CAP_PROP_FRAME_WIDTH) / downsample;
-        int height = (int)cap.get(CV_CAP_PROP_FRAME_HEIGHT) / downsample;
-        array.resize(width * height);
-        for(auto& a: array)
-            a.resize(tWindow * 3);
 
-        cv::Size dsize(width, height);
-
-        const int stride = width * height * 3;
-        for(auto fid=0; fid<tWindow; ++fid){
-            Mat frame;
-            if(cap.read(frame))
-                break;
-            cv::resize(frame, frame, dsize);
-            //cvtColor(frame, frame, CV_BGR2Luv);
-            cvtColor(frame, frame, CV_BGR2RGB);
-            const uchar* pFrame = frame.data;
-            for(auto i=0; i<width * height; ++i){
-                array[i][fid*3] = pFrame[3*i];
-                array[i][fid*3+1] = pFrame[3*i+1];
-                array[i][fid*3+2] = pFrame[3*i+2];
-            }
-        }
-
-	    return dsize;
+    void FeatureConstructor::normalizel2(std::vector<float> &array) const {
+        const float epsilon = 1e-3;
+        float sqsum = 0.0;
+        for(auto f: array)
+            sqsum += f * f;
+        if(sqsum < epsilon)
+            return;
+        for(auto& f: array)
+            f /= std::sqrt(sqsum);
     }
 
-    void extractFeatureRGBCat(const std::vector<std::vector<float> >& array, const cv::Size& dims, std::vector<std::vector<float> >& features, const int kBin){
-        CHECK(!array.empty());
-        features.resize(array.size());
-        const float binUnit = 512 / (float)kBin;
-        for(auto pid=0; pid < array.size(); ++pid){
-            CHECK_EQ((int)array[pid].size() % 3, 0);
-            features[pid].resize((size_t)kBin * 3);
-            for(auto t=0; t<array[pid].size()/3 - 1; ++t){
-                for(auto c=0; c<3; ++c){
-                    float diff = array[pid][(t+1)*3+c] - array[pid][t*3+c];
-                    int bid = floor((diff + 256) / binUnit);
-                    CHECK_LT(bid, kBin);
-                    features[pid][kBin*c+bid] += 1.0;
+    void RGBCat::constructFeature(const std::vector<float>& array, std::vector<float>& feat) const{
+        CHECK_EQ((int)array.size() % 3, 0);
+        feat.resize((size_t)kBin * 3 + kBinIntensity, 0.0f);
+        vector<float> feat_diff((size_t)kBin*3, 0.0f);
+        vector<float> feat_intensity((size_t)kBinIntensity, 0.0f);
+
+        Vector3f RGB2Gray(0.299, 0.587, 0.114);
+        for(auto t=0; t<array.size()/3 - 1; ++t){
+            Vector3f pix1(array[t*3],array[t*3+1],array[t*3+2]);
+            Vector3f pix2(array[(t+1)*3],array[(t+1)*3+1],array[(t+1)*3+2]);
+
+            //intensity
+            float intensity = pix1.dot(RGB2Gray);
+            int bidInt = floor(intensity / 256);
+            CHECK_LT(bidInt, kBinIntensity);
+            feat_intensity[bidInt] += 1.0;
+
+            //color change
+            Vector3f diff = pix2 - pix1;
+            if(diff.norm() >= min_diff) {
+                for (auto c = 0; c < 3; ++c) {
+                    int bid = floor((diff[c] + 256) / binUnit);
+                    CHECK_LT(kBin * c + bid, feat.size());
+                    feat_diff[kBin * c + bid] += 1.0;
                 }
             }
-            //normalize
-            float sqsum = 0.0;
-            for(auto bid=0; bid<features[pid].size(); ++bid)
-                sqsum += features[pid][bid] * features[pid][bid];
-            CHECK_GT(sqsum, 0.1);
-            for (auto bid = 0; bid < features[pid].size(); ++bid)
-                features[pid][bid] /= sqsum;
         }
+        //normalize, cut and renormalize
+        normalizel2(feat_intensity);
+        normalizel2(feat_diff);
+        for(auto& f: feat_intensity){
+            if(f < cut_thres)
+                f = 0;
+        }
+        for(auto& f: feat_diff){
+            if(f < cut_thres)
+                f = 0;
+        }
+        normalizel2(feat_intensity);
+        normalizel2(feat_diff);
+
+        feat.insert(feat.end(), feat_diff.begin(), feat_diff.end());
+        feat.insert(feat.end(), feat_intensity.begin(), feat_intensity.end());
     }
+
+    void DataSet::dumpData_libsvm(const std::string &path) const {
+        ofstream fout(path.c_str());
+        CHECK(fout.is_open());
+        char buffer[1024] = {};
+        for(auto i=0; i<features.size(); ++i){
+            for(auto j=0; j<features[i].size(); ++j){
+                fout << i;
+                for(auto k=0; k<features[i][j].size(); ++k){
+                    if(features[i][j][k] > 0){
+                        sprintf(buffer, " %d:%.3f", k, features[i][j][k]);
+                        fout << buffer;
+                    }
+                }
+                fout << endl;
+            }
+        }
+        fout.close();
+    }
+
+    void DataSet::appendDataSet(const DataSet& newData){
+        const FeatureSet& newFeature = newData.getFeatures();
+        if(features.empty())
+            features.resize(newFeature.size());
+        CHECK_EQ(features.size(), newFeature.size());
+        for(auto i=0; i<features.size(); ++i)
+            features[i].insert(features[i].end(), newFeature[i].begin(), newFeature[i].end());
+    }
+
+    namespace Feature {
+        cv::Size importData(const std::string& path, std::vector<std::vector<float> >& array, const int downsample, const int tWindow){
+            VideoCapture cap(path);
+            CHECK(cap.isOpened());
+            int width = (int)cap.get(CV_CAP_PROP_FRAME_WIDTH) / downsample;
+            int height = (int)cap.get(CV_CAP_PROP_FRAME_HEIGHT) / downsample;
+            array.resize(width * height);
+            for(auto& a: array)
+                a.resize(tWindow * 3);
+
+            cv::Size dsize(width, height);
+
+            const int stride = width * height * 3;
+            for(auto fid=0; fid<tWindow; ++fid){
+                Mat frame;
+                if(cap.read(frame))
+                    break;
+                cv::resize(frame, frame, dsize);
+                //cvtColor(frame, frame, CV_BGR2Luv);
+                cvtColor(frame, frame, CV_BGR2RGB);
+                const uchar* pFrame = frame.data;
+                for(auto i=0; i<width * height; ++i){
+                    array[i][fid*3] = pFrame[3*i];
+                    array[i][fid*3+1] = pFrame[3*i+1];
+                    array[i][fid*3+2] = pFrame[3*i+2];
+                }
+            }
+
+            return dsize;
+        }
+
+
+        void extractFeature(const std::vector<std::vector<float> > &array, const cv::Size &dims, const cv::Mat &gt, DataSet& dataset,
+                            const int kBin, const float min_diff, const FeatureType method) {
+            CHECK(!array.empty());
+            vector<vector<int> > pixInds;
+            const int negStride = 4;
+            const int width = dims.width;
+            const int height = dims.height;
+
+            DataSet::FeatureSet& samples = dataset.getFeatures();
+
+            if (gt.data) {
+                CHECK_EQ(gt.size(), dims);
+                pixInds.resize(2);
+                for (auto y = 0; y < height; y += negStride) {
+                    for (auto x = 0; x < width; x += negStride) {
+                        if (gt.at<uchar>(y, x) < (uchar)10)
+                            pixInds[0].push_back(y * width + x);
+                    }
+                }
+                for (auto y = 0; y < height; ++y) {
+                    for (auto x = 0; x < width; ++x) {
+                        if (gt.at<uchar>(y, x) > (uchar)200)
+                            pixInds[1].push_back(y * width + x);
+                    }
+                }
+                samples.resize(2);
+                printf("kPositive: %d, kNegative: %d\n", (int)pixInds[1].size(), (int)pixInds[0].size());
+            } else {
+                pixInds.resize(1);
+                for (auto id = 0; id < width * height; ++id)
+                    pixInds[0].push_back(id);
+                samples.resize(1);
+            }
+
+            //feature constructor
+            shared_ptr<FeatureConstructor> featureConstructor(NULL);
+            switch (method){
+                case RGB_CAT:
+                    featureConstructor.reset(new RGBCat(kBin, min_diff));
+                    break;
+                default:
+                    CHECK(true) << "Unsupported method";
+            }
+
+            for (auto i = 0; i < samples.size(); ++i) {
+                samples[i].resize(pixInds[i].size());
+                for(auto pid=0; pid<pixInds[i].size(); ++pid) {
+                    featureConstructor->constructFeature(array[pixInds[i][pid]], samples[i][pid]);
+                }
+            }
+        }
+
+    }
+
+
 
 }//namespace dynamic_stereo
--- code/extractFeature/extractfeature_cli.cpp
+++ code/extractFeature/extractfeature_cli.cpp
@@ -10,18 +10,61 @@ using namespace dynamic_stereo;
 
 DEFINE_int32(downsample, 4, "downsample ratio");
 DEFINE_int32(tWindow, 100, "tWindow");
+DEFINE_bool(trainData, true, "training data");
 DEFINE_int32(kBin, 8, "kBin");
+DEFINE_double(min_diff, 10, "min_diff");
 
-int main(int argc, char** argv){
-    if(argc < 2){
-        cerr << "Usage: tempMeanshift_cli <path-to-data>" << endl;
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        cerr << "Usage: extractFeature <path-to-data>" << endl;
         return 1;
     }
-    google::ParseCommandLineFlags(&argc, &argv, true);
     google::InitGoogleLogging(argv[0]);
-    string file_path(argv[1]);
+    google::ParseCommandLineFlags(&argc, &argv, true);
 
+    CHECK_EQ(argc, 3);
 
+    DataSet data_all;
+
+    if (FLAGS_trainData) {
+        string list_path = string(argv[1]);
+        size_t dashPos;
+        for(dashPos = list_path.size()-1; dashPos>=0 ;--dashPos){
+            if(list_path[dashPos] == '/')
+                break;
+        }
+        string directory = list_path.substr(0, dashPos);
+        directory.append("/");
+        ifstream fin(list_path.c_str());
+        //cout << "list path:" << list_path << endl;
+        CHECK(fin.is_open());
+        while (true) {
+            DataSet curData;
+            string video_path, gt_path;
+            fin >> video_path >> gt_path;
+            cout << "video path: " << video_path << " mask path: " << gt_path << endl;
+            if (video_path.empty() || gt_path.empty())
+                break;
+            vector<vector<float> > data;
+            printf("Reading...\n");
+            cv::Size dim = Feature::importData(directory + video_path, data, FLAGS_downsample, FLAGS_tWindow);
+            Mat gt = imread(directory + gt_path, false);
+            CHECK(gt.data);
+            cv::resize(gt,gt,dim,0,0,INTER_NEAREST);
+            printf("Extracting...\n");
+            Feature::extractFeature(data, dim, gt, curData, FLAGS_kBin, (float)FLAGS_min_diff, Feature::RGB_CAT);
+            data_all.appendDataSet(curData);
+        }
+    } else {
+        vector<vector<float> > data;
+        printf("Preparing testing data: %s\n", argv[1]);
+        cv::Size dim = Feature::importData(string(argv[1]), data, FLAGS_downsample, FLAGS_tWindow);
+        printf("Extracting...\n");
+        Feature::extractFeature(data, dim, Mat(), data_all, FLAGS_kBin, (float)FLAGS_min_diff, Feature::RGB_CAT);
+    }
+
+    printf("Saving...\n");
+    data_all.dumpData_libsvm(string(argv[2]));
 
     return 0;
 }
