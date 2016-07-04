627bae0dc1f55107bad6f030f91f49fca9aef206
higerra <higerra@126.com>
2016-07-04 12:58:50 +0800

    Hang

--- code/run_segmentation.py
+++ code/run_segmentation.py
@@ -3,14 +3,15 @@ import subprocess
 datasets = ['casino2', 'casino3', 'casino4', 'casino5', 'casino6', 'casino7', 'casino8', 'statue1', 'vegas29', 'vegas32', 'vegas33']
 testFrame = ['00100', '00200']
 
-video_segment_path = '/home/yanhang/Documents/research/external_code/video_segment/seg_tree_sample/build/seg_tree_sample'
+root_path = '~/Documents/research/DynamicStereo/data'
+video_segment_path = '~/Documents/research/external_code/video_segment/seg_tree_sample/build/seg_tree_sample'
 
 for data in datasets:
     for tf in testFrame:
-        command = 'ffmpeg -i ../data/data_{}/midres/prewarp/prewarpb{}_%05d.jpg -vcodec h264 -qp 0 data/data_{}/midres/prewarp/prewarpb{}.mp4'.format(data, tf, data, tf)
+        command = 'ffmpeg -i {}/data_{}/midres/prewarp/prewarpb{}_%05d.jpg -vcodec h264 -qp 0 {}/data_{}/midres/prewarp/prewarpb{}.mp4'.format(root_path, data, tf, root_path, data, tf)
         print command
         subprocess.check_call(command, shell=True)
 
-        command = '{} ../data/data_{}/midres/prewarp/prewarpb{}.mp4 --write_to_file --render_and_save'.format(video_segment_path, data, tf)
+        command = '{} {}/data_{}/midres/prewarp/prewarpb{}.mp4 --write_to_file --render_and_save'.format(video_segment_path, root_path, data, tf)
         print command
         subprocess.check_call(command, shell=True)
--- code/run_stereo.py
+++ code/run_stereo.py
@@ -3,10 +3,11 @@ import subprocess
 datasets = ['casino2', 'casino3', 'casino4', 'casino5', 'casino6', 'casino7', 'casino8', 'statue1', 'shopping1', 'shopping2', 'vegas29', 'vegas32', 'vegas33']
 testFrame = ['00100','00200']
 
-stereo_path = '/home/yanhang/Documents/research/DynamicStereo/code/build/DynamicStereo'
+root_path = '~/Documents/research/DynamicStereo/data'
+stereo_path = '~/Documents/research/DynamicStereo/code/build/DynamicStereo/DynamicStereo'
 
 for data in datasets:
     for tf in testFrame:
-        command = '{} ../data/data_{} --testFrame={} --resolution=128'.format(stereo_path, data, tf)
+        command = '{} {}/data_{} --testFrame={} --resolution=128'.format(stereo_path, root_path, data, tf)
         print command
         subprocess.check_call(command, shell=True)    
