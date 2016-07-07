3529fccbc6375f7d159f31474f4ba57c4a134bc1
higerra <higerra@126.com>
2016-07-07 11:34:57 +0800

    Hang

---
+++ code/scripts/run_training_segment.py
@@ -0,0 +1,20 @@
+import subprocess
+import argparse
+from os.path import dirname
+
+parser = argparse.ArgumentParser()
+parser.add_argument('input')
+
+args = parser.parse_args()
+listfile = open(args.input)
+
+seg_binary_dir = "~/Documents/research/external_code/video_segment/seg_tree_sample/build/seg_tree_sample"
+
+samples = listfile.readlines()
+path = dirname(args.input)
+print path
+
+for sample in samples:
+    command = "{} --input_file={}/{} --write_to_file --render_and_save".format(seg_binary_dir, path, sample)
+    print command
+    subprocess.call(command, shell=True)
