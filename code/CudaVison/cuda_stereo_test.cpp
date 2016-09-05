fc3bfaef7d6bc8d2b2fe1844a039bde3e5a67b9d
Hang Yan <yanhangxiong@gmail.com>
2016-09-05 18:11:58 -0500

    Hang

--- code/CudaVison/cuda_stereo_test.cpp
+++ code/CudaVison/cuda_stereo_test.cpp
@@ -14,9 +14,6 @@ using namespace std;
 
 TEST(CudaStereo, RadialDistortion){
     const Vector2f undistorted_point = Vector2f::Random();
-
-    cout << "Undistorted point: " << undistorted_point.transpose() << endl;
-
     Vector2f distorted_point, distorted_point_theia;
     float k1 = 0, k2 = 0;
     RadialDistortPoint(undistorted_point.x(), undistorted_point.y(),
@@ -24,9 +21,6 @@ TEST(CudaStereo, RadialDistortion){
 
     theia::RadialDistortPoint<float>(undistorted_point.x(), undistorted_point.y(),
                       k1, k2, distorted_point_theia.data(), distorted_point_theia.data() + 1);
-    printf("k1: %.3f, k2: %.3f\n", k1, k2);
-    cout << "Distorted cuda: " << distorted_point.transpose() << endl;
-    cout << "Distorted theia: " << distorted_point_theia.transpose() << endl;
     EXPECT_NEAR((distorted_point - distorted_point_theia).norm(), 0.0, std::numeric_limits<float>::min());
 
     k1 = 0.1; k2 = 0;
@@ -36,9 +30,6 @@ TEST(CudaStereo, RadialDistortion){
     theia::RadialDistortPoint<float>(undistorted_point.x(), undistorted_point.y(),
                               k1, k2, distorted_point_theia.data(), distorted_point_theia.data() + 1);
 
-    printf("k1: %.3f, k2: %.3f\n", k1, k2);
-    cout << "Distorted cuda: " << distorted_point.transpose() << endl;
-    cout << "Distorted theia: " << distorted_point_theia.transpose() << endl;
     EXPECT_NEAR((distorted_point - distorted_point_theia).norm(), 0.0, std::numeric_limits<float>::min());
 
     k1 = 0.1, k2 = 0.05;
@@ -48,12 +39,29 @@ TEST(CudaStereo, RadialDistortion){
     theia::RadialDistortPoint<float>(undistorted_point.x(), undistorted_point.y(),
                               k1, k2, distorted_point_theia.data(), distorted_point_theia.data() + 1);
 
-    printf("k1: %.3f, k2: %.3f\n", k1, k2);
-    cout << "Distorted cuda: " << distorted_point.transpose() << endl;
-    cout << "Distorted theia: " << distorted_point_theia.transpose() << endl;
     EXPECT_NEAR((distorted_point - distorted_point_theia).norm(), 0.0, std::numeric_limits<float>::min());
 }
 
 TEST(CudaStereo, ProjectPointToImage){
+    float k1 = 0.1, k2 = 0.05;
+    Vector4f spt = Vector4f::Random();
+    spt[3] = 1.0;
+
+    //generate a theia camera, and convert to CudaCamera
+    theia::Camera cam;
+    cam.SetFocalLength(600);
+    cam.SetPrincipalPoint(300, 400);
+    cam.SetRadialDistortion(0.01, 0.001);
+    cam.SetPosition(Vector3d::Ones());
+    Vector3d gt_angle_axis(0.3,0.7,0.4);
+    Matrix3d gt_rotation_matrix;
+    ceres::AngleAxisToRotationMatrix(gt_angle_axis.data(), gt_rotation_matrix.data());
+    cam.SetOrientationFromRotationMatrix(gt_rotation_matrix);
+
+    CudaCamera cudacam;
+    theia::Matrix3x4d extrinsic_theia;
+    cam.GetProjectionMatrix(&extrinsic_theia);
+
+    Vector2d pixel_cuda, pixel_theia;
 
 }
